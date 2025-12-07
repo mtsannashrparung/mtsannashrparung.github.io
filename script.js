b1e9431c6d7628e243deb3306e82583d88691b76
dzms ddd <ahmadzaeladi@gmail.com>
2025-12-07 13:08:29 +0700

    feat: update  terbaru

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const btn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+
 // Smooth scroll button effect (opsional)
   const navbar = document.getElementById("navbar");
 
